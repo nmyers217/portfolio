532e9d224c2c92b98453cdda3093a08c4d47a3c1
nickmyers217 <nickbmyers217@gmail.com>
2016-07-25 03:11:57 -0700

    Prototyped out the first version of the site.
    Right now the entire site is residing in the root component.
    
    There are also many bad practices in place with script tags.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-
-import { NavbarComponent } from './navbar';
+import { Component
+       , OnInit
+       , AfterViewInit
+       , ElementRef
+       } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [NavbarComponent],
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Under Construction!';
+
+  constructor(private elementRef: ElementRef) {};
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // TODO: Is there a better way to load these modules
+    // into the DOM after the component?
+    // They are pretty old and don't follow any module conventions
+    this.addScriptToDOM('js/particles.min.js');
+    this.addScriptToDOM('js/particlesRun.js');
+
+    // TODO: Pull this code into the component
+    // this will improve encapsulation and
+    // seperation of concerns blah blah blah
+    this.addScriptToDOM('js/main.js');
+  }
+
+  private addScriptToDOM(path: string): void {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = path;
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
