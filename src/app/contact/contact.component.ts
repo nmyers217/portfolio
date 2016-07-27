c06833607ed565acf2a14ab3bd275be8fd3d2fe8
nickmyers217 <nickbmyers217@gmail.com>
2016-07-27 04:48:44 -0700

    Implemented the http service for the contact form.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ControlGroup, Validators } from '@angular/common';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { ContactService } from './contact.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-contact',
-  templateUrl: 'contact.component.html',
-  styleUrls: ['contact.component.css']
+    moduleId: module.id,
+    selector: 'app-contact',
+    templateUrl: 'contact.component.html',
+    styleUrls: ['contact.component.css'],
+    providers: [HTTP_PROVIDERS, ContactService]
 })
 export class ContactComponent implements OnInit {
+    contactForm: ControlGroup;
 
-  constructor() { }
+    constructor(private fb:FormBuilder, private cs: ContactService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.contactForm = this.fb.group({
+            subject: ['', Validators.required],
+            name: ['', Validators.required],
+            email: ['', Validators.required],
+            message: ['', Validators.required],
+            phone: ['']
+        });
+    }
 
+    sendMessage(e) {
+        this.cs.formSpreePost(this.contactForm.value)
+            .subscribe(res => console.log(res));
+    }
 }
