a94ee99c9a06665eb648ff44237987910f56c72a
NickMyers217 <nickbmyers217@gmail.com>
2020-01-31 21:56:43 -0600

    Feat: huge progress towards the gatsby 2 port

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import Particles from 'react-particles-js';
 import $ from 'jquery';
 
-import Particles from 'react-particles-js';
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "Me.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="Profile" />
+  );
+};
 
 class Hero extends Component {
   constructor(props) {
@@ -33,22 +52,35 @@ class Hero extends Component {
     window.removeEventListener('resize', this.updateDimensions);
   }
 
-  render() { 
+  render() {
     const { width, height } = this.state;
+
     return (
-      <div className="jumbotron jumbotron-main" id="home" ref={jumbo => this.jumbo = jumbo} style={{width, height}}>
+      <div
+        className="jumbotron jumbotron-main"
+        id="home"
+        ref={jumbo => (this.jumbo = jumbo)}
+        style={{ width, height }}
+      >
         <div id="particles-js">
           <Particles params={this.props.config} wdith={width} height={height} />
         </div>
         <div className="container center-vertically-holder">
           <div className="center-vertically">
-            <div id="scaleReveal" className="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3 text-center">
+            <div
+              id="scaleReveal"
+              className="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3 text-center"
+            >
               <div className="big-logo scaleReveal">
-                <img src="images/Me.jpg" alt="Profile" />
+                <Image />
               </div>
-              <h1 className="scaleReveal">Hi, I'm Nick</h1>
+              <h1 className="scaleReveal">Hi, I'm Nick 👋</h1>
               <hr className="bottomReveal" />
-              <p className="bottomReveal">I'm a Full Stack Web Developer in the Houston, TX area who is very passionate about programming and technology. In my free time I can be found watching scifi movies and envisioning the future.</p>
+              <p className="bottomReveal">
+                I'm a Full Stack Web Developer in the Houston, TX area with 4+
+                years of industry experience. I have am always curious and
+                driven to explore programming and technology 🤖
+              </p>
               <a href="#skills" data-id="skills" className="scroll-link">
                 <div className="scroll-indicator rotateBottomReveal">
                   <span className="ion-mouse"></span>
